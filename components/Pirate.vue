2e3f80769ba8be4ead30c8781098ab6b6ba22437
Fariz313 <fariz313.akbar@gmail.com>
2022-03-28 09:21:18 +0700

    update content

--- components/Pirate.vue
+++ components/Pirate.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="main-page py-5" style="background-color:#7787BF">
-    <img class="rightAbsolute" src="~assets/jmpl.svg" alt="" srcset="">
+  <div class="main-page py-5" style="background-color: #7787bf">
+    <img class="rightAbsolute" src="~assets/jmpl.svg" alt="" srcset="" />
     <h1>The Pirate Funnel</h1>
     <h2 class="mt-4">Awareness</h2>
-    <p>Sertifikasi digital sebagai solusi mudah untuk sertifikasi profesi</p>
+    <p>Training digital sebagai solusi mudah untuk training profesi</p>
     <h2 class="mt-4">Acquisition</h2>
-    <p>Bekerjasama dengan BNSP sebagai partner yang tepat..</p>
+    <p>Bekerjasama dengan BNSP dan LSP sebagai partner yang tepat..</p>
     <h2 class="mt-4">Activation</h2>
     <p>
-      Meberikan layanan sertifikasi yang dapat diakses dari mana saja, kapanpun,
+      Meberikan layanan training yang dapat diakses dari mana saja, kapanpun,
       dan fleksibel.
     </p>
     <h2 class="mt-4">Retention</h2>
-    <p>Menyediakan sertifikasi digital tanpa batas kuota peserta.</p>
+    <p>Menyediakan training digital tanpa batas kuota peserta.</p>
     <h2 class="mt-4">Revenue</h2>
     <p>Memiliki legalisasi dan akredibelitas yang terpercaya.</p>
   </div>
 </template>
 <style scoped>
-.rightAbsolute{
-position: absolute;
-right: 0;
+.rightAbsolute {
+  position: absolute;
+  right: 0;
 }
 </style>
